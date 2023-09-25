e53518eda76a3dfc05d8cd09947a489327074559
Nathan Hartojo <nathanhartojo@gmail.com>
2023-09-25 21:00:27 +0000

    admin dashboard has list of all users displayed

--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,19 @@ Rails.application.routes.draw do
     get 'admin', to: 'admin#index'
     get 'admin/users'
   end
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
+
   root to: "items#index"
   resources :comments
+
   resources :posts do
     resources :comments
   end
+  
   resources :items do
     member do
       get :delete
