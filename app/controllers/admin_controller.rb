052c88a364194efd9e11e9d9037c405cc26fb556
Nathan Hartojo <nathanhartojo@gmail.com>
2023-09-26 03:52:13 +0000

    added more tests for coverage

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,12 +1,23 @@
 class AdminController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_admin
+
   def index
   end
   
   def users
-    @users = User.all 
+    @users = User.all
   end
-
+  
   def transactions
     @transactions = Transaction.all
+  end
+  
+  private
+  
+  def ensure_admin
+    unless current_user && current_user.admin?
+      redirect_to root_path, alert: "Access denied!"
+    end
   end
 end
