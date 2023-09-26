5e7c2d895c277ba6372a9efaa1f994a21d756595
Nathan Hartojo <nathanhartojo@gmail.com>
2023-09-26 03:56:30 +0000

    update user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :user do
       fname {"Test"}
       lname {"Admin"}
-      email { "admin@gmail.com" }
+      email { "admintest@gmail.com" }
       password { "password" }
       password_confirmation { "password" }
       role { "Member" } 
