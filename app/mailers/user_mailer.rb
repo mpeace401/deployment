a24e6050705d9e7e8a88462733d1f4be212662b3
mpeace401 <marissa.a.peace@gmail.com>
2023-10-12 16:27:00 -0500

    Added welcome_email method and view

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from 'notfications@radioclub.com'
+
+    def welcome_email
+        @user = params[:user]
+        @url = 'http://localhost:3000/users/sign_in' #UPDATE WITH APP LINK
+        mail(to: @user.email, subject: 'Welcome to the TAMU Amateur Radio Club Inventory')
+    end
 end
